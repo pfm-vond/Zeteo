73120081a01c49723b5a93a93932d258e8b28758
pfm-vond <pierre.vonderscher@gmail.com>
2020-05-03 12:07:21 +0000

    Automated Release 17

---
+++ dist/dist/precache-manifest.447d7842ab370c0131a3744a43d42453.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9494ba1efa88eb023114",
+    "url": "/css/app.bdda2b11.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "30d71824d3ba6e21a9b39e6cf3ae2ad2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "652b3522bac4f7835f28",
+    "url": "/js/about.fa39ad0d.js"
+  },
+  {
+    "revision": "9494ba1efa88eb023114",
+    "url": "/js/app.ec553242.js"
+  },
+  {
+    "revision": "c05cd9eff64a70ed227e",
+    "url": "/js/chunk-vendors.9101d7bd.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
