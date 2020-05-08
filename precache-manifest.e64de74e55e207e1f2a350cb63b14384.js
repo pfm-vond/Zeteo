1de5cfc40526ea21a568da221f59ca8ad0bb24c1
pfm-vond <pierre.vonderscher@gmail.com>
2020-05-08 13:55:40 +0000

    Automated Release 31

---
+++ precache-manifest.e64de74e55e207e1f2a350cb63b14384.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb0af4f439e3ecf76f8",
+    "url": "/css/app.1817dff2.css"
+  },
+  {
+    "revision": "ee437ea7ab44539c4223",
+    "url": "/css/chunk-vendors.ece97acf.css"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "03455dcf0164cfb792f4f9cbe81c7cab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceb0af4f439e3ecf76f8",
+    "url": "/js/app.3342c9ae.js"
+  },
+  {
+    "revision": "ee437ea7ab44539c4223",
+    "url": "/js/chunk-vendors.34ebc05d.js"
+  },
+  {
+    "revision": "c6c1fd1f2162d917da3f93f170b25708",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.447d7842ab370c0131a3744a43d42453.js"
+  "/precache-manifest.e64de74e55e207e1f2a350cb63b14384.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "app"});
